2d942bd181723485adb8d9850780f190d7c92348
Tomasz Borek <Tomasz.Borek@gmail.com>
2023-11-12 11:42:44 +0100

    Cel roczny: technologie, jakich używam opublikowane.
    Przy okazji testy DNSa strony i podmiana URLi na HTTPS.

---
+++ lafk.pl_dns_test.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+firefox https://github.com/LIttleAncientForestKami/LIttleAncientForestKami.github.io/settings/pages http://lafk.pl http://www.lafk.pl/ https://lafk.pl https://www.lafk.pl https://stackoverflow.com/questions/54059217/how-to-fix-domain-does-not-resolve-to-the-github-pages-server-error-in-github https://www.aftermarket.pl/Domain/DNS/List/?domain=lafk.pl https://docs.github.com/en/pages/configuring-a-custom-domain-for-your-github-pages-site
